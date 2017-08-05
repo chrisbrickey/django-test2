a299a594e2b5daa89303bedb5d1637b35b94831e
chrisbrickey <cbrickey1@gmail.com>
2017-08-05 15:31:42 -0700

    initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrisbrickey/quickfit/django-test2/virtual-django-test2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
